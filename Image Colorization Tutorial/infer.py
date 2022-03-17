63a2aba56ae46f3875e626c49567055bd6bf3aa5
Tanishka Marrott <78227704+TanishkaMarrott@users.noreply.github.com>
2022-03-17 21:53:00 +0530

    Update infer.py

--- Image Colorization Tutorial/infer.py
+++ Image Colorization Tutorial/infer.py
@@ -1,28 +1,29 @@
-## Update: 8th Jan, 2022
-## this file is supposed to give you a general idea on how to
-## use the pre-trained model for colorizing B&W images. This
-## file still needs development.
-
 import PIL
 import torch
 from matplotlib import pyplot as plt
 from torchvision import transforms
+
+from PIL import Image
+import cv2
+import IPython
+from IPython.display import Image
 
 from models import MainModel
 from utils import lab_to_rgb
 
 if __name__ == '__main__':
     model = MainModel()
+    
     # You first need to download the final_model_weights.pt file from my drive
     # using the command: gdown --id 1lR6DcS4m5InSbZ5y59zkH2mHt_4RQ2KV
+    
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.load_state_dict(
         torch.load(
             "final_model_weights.pt",
-            map_location=device
         )
     )
-    path = "Path to your black and white image"
+     path = "/content/img_9.jfif"
     img = PIL.Image.open(path)
     img = img.resize((256, 256))
     # to make it between -1 and 1
@@ -31,4 +32,4 @@ if __name__ == '__main__':
     with torch.no_grad():
         preds = model.net_G(img.unsqueeze(0).to(device))
     colorized = lab_to_rgb(img.unsqueeze(0), preds.cpu())[0]
-    plt.imshow(colorized)
+    colorize_image(path)
